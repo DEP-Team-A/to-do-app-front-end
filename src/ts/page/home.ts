62c4e904992a5740c9dbec4829ec97bd64b2367b
Dulanga Wimalagunasekara <dulangawimalagunasekara@gmail.com>
2022-05-09 01:11:42 +0530

    Create the swap funtion of the left table

---
+++ src/ts/page/home.ts
@@ -0,0 +1,31 @@
+const tblLeft = document.querySelector<HTMLTableElement>('#tbl-left')!;
+const tblRight = document.querySelector<HTMLTableElement>('#tbl-right')!;
+
+
+tblLeft.addEventListener('click',removeTask);
+
+function removeTask(e:Event){
+    if ((e.target as HTMLElement).classList.contains('fa-hand-point-right')){
+        const elm = e.target as HTMLElement;
+        let row = elm.closest('tr')!;
+        let td = row.querySelector('td')!;
+        let nodes = td.querySelectorAll('div');
+        const date=nodes[0].innerText;
+        const des=nodes[1].innerText;
+        const note=nodes[2].innerText;
+        row.remove();
+
+        const newRow = document.createElement('tr');
+        newRow.innerHTML=`<td>
+                           <div class="isbn">${date}</div>
+                           <div class="book-name text-bold">${des}</div>
+                           <div class="book-author">${note}</div>
+                          </td>
+                           <td class="trash">
+                            <i class="fas fa-hand-point-left"></i>
+                           </td>`;
+
+        tblRight.querySelector('tbody')!.append(newRow);
+
+    }
+}
